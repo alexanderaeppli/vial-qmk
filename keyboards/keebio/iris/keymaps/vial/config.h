0708f19826e1e0d1fb2c1c84ebbf77f4f01da347
Alexander Aeppli <git@alexanderaeppli.ch>
2025-04-13 12:46:21 +0200

    initial config for keebio iris rev 8

--- keyboards/keebio/iris/keymaps/vial/config.h
+++ keyboards/keebio/iris/keymaps/vial/config.h
@@ -20,6 +20,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 #define EE_HANDS
 
+// Custom change to add more layers and macros
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define DYNAMIC_KEYMAP_MACRO_COUNT 64
+
 // Added by Brian C. Arnold to support Vial.
 #define VIAL_KEYBOARD_UID {0xBC, 0x99, 0xF2, 0x61, 0x45, 0x7C, 0xC8, 0xB9}
 #define VIAL_UNLOCK_COMBO_ROWS {0,9}
